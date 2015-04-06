f851cadb53286e3bc7e83de59d553ce69920a1f6
Pomin Wu <pomin5@gmail.com>
2015-04-06 15:15:11 +0800

    start project

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var co = require('co')
+
+module.exports = middleagent = function () {
+  return agent
+}
+
+agent.use = function (mw) {
+}
+
+agent.get = function (url) {
+}
+
+agent.post = function (url) {
+}
